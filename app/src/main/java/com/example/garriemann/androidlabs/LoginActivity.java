dfee0d495e9a31a87f4b9546c16d673fb388019a
Garrie Mann <gursewak.singh001@outlook.com>
2018-03-06 19:34:49 -0500

    Lab 04

--- app/src/main/java/com/example/garriemann/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/garriemann/androidlabs/LoginActivity.java
@@ -1,74 +1,150 @@
 package com.example.garriemann.androidlabs;
 
+
 import android.app.Activity;
+
 import android.content.Context;
+
 import android.content.Intent;
+
 import android.content.SharedPreferences;
+
 import android.os.Bundle;
+
 import android.util.Log;
+
 import android.view.View;
+
 import android.widget.Button;
+
 import android.widget.EditText;
+
 import android.widget.TextView;
+
+
 
 public class LoginActivity extends Activity {
 
-  protected static final String ACTIVITY_NAME = "Login Activity";
 
-   EditText editLogin;
-    Button lgnButton = (Button) findViewById(R.id.loginButton);
+
+    protected static final String ACTIVITY_NAME = "Login Activity";
+
+
+
+    EditText edtLogin;
+
+    TextView loginText;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
 
-        Log.i(ACTIVITY_NAME,"In onCreate()");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
-        editLogin = findViewById(R.id.email);
+        Log.i(ACTIVITY_NAME,"In onCreate()");
 
-        final SharedPreferences StoredEmails = getSharedPreferences("login", Context.MODE_PRIVATE);
-        editLogin.setText(StoredEmails.getString("DefaultEmail", "email@domain.com"));
-        lgnButton.setOnClickListener(new View.OnClickListener() {
+        Button loginButton = (Button) findViewById(R.id.button2);
+
+        edtLogin = (EditText) findViewById(R.id.email);
+
+        final SharedPreferences sharedPreferences = getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        edtLogin.setText(sharedPreferences.getString("DefaultEmail", "email@domain.com"));
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
+
             public void onClick(View v) {
-                SharedPreferences.Editor editor = StoredEmails.edit();
-                editor.putString("DefaultEmail", editLogin.getText().toString());
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("DefaultEmail", edtLogin.getText().toString());
                 editor.commit();
 
                 Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+
                 startActivity(intent);
+
+
+
             }
+
         });
+
+
+
     }
 
+
+
     @Override
+
     protected void onStart() {
+
         super.onStart();
+
         Log.i(ACTIVITY_NAME, "In onStart()");
+
+
+
     }
 
+
+
     @Override
+
     protected void onResume() {
+
         super.onResume();
+
         Log.i(ACTIVITY_NAME,"In onResume()");
+
+
+
     }
 
+
+
     @Override
+
     protected void onStop() {
+
         super.onStop();
+
         Log.i(ACTIVITY_NAME,"In onStop()");
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(ACTIVITY_NAME,"In onDestroy()");
+
+
+
     }
 
+
+
     @Override
+
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
+
+
+
+    }
+
+
+
+    @Override
+
     protected void onPause() {
+
         super.onPause();
+
         Log.i(ACTIVITY_NAME,"In onPause()");
+
+
+
     }
 
 }
