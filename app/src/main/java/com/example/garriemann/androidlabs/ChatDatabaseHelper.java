0f9d12e97c24f8d61981e08f642f5a57f973bd22
Garrie Mann <gursewak.singh001@outlook.com>
2018-03-20 17:58:06 -0400

    Lab 06

--- app/src/main/java/com/example/garriemann/androidlabs/ChatDatabaseHelper.java
+++ app/src/main/java/com/example/garriemann/androidlabs/ChatDatabaseHelper.java
@@ -12,9 +12,9 @@ import android.util.Log;
 
 public class ChatDatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int VERSION_NUM = 2;
-    public static final String DATABASE_NAME= "message2.db";
-    public static final String TABLE_NAME= "chat";
+    public static final int VERSION_NUM = 3;
+    public static final String DATABASE_NAME= "messages.db";
+    public static final String TABLE_NAME = "chat";
     public final static String KEY_ID="id_key";
     public final static String KEY_MESSAGE="message_key";
 
--- app/src/main/java/com/example/garriemann/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/garriemann/androidlabs/StartActivity.java
@@ -14,24 +14,24 @@ public class StartActivity extends Activity {
     protected static final String ACTIVITY_NAME = "Start Activity";
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         Log.i(ACTIVITY_NAME,"In onCreate()");
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
-
             public void onClick(View v) {
                 Intent intent = new Intent (StartActivity.this,ListItemsActivity.class);
                 startActivityForResult(intent,5);
             }
 
         });
-        Button button1 = (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -41,10 +41,18 @@ public class StartActivity extends Activity {
 
         });
 
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (StartActivity.this,WeatherForecast.class);
+                startActivityForResult(intent,5);
+            }
+        });
     }
 
     @Override
-
     protected void onResume(){
         super.onResume();
         Log.i(ACTIVITY_NAME,"In onResume()");
@@ -52,7 +60,6 @@ public class StartActivity extends Activity {
     }
 
     @Override
-
     protected void onStart(){
         super.onStart();
         Log.i(ACTIVITY_NAME,"In onStart()");
@@ -60,21 +67,18 @@ public class StartActivity extends Activity {
     }
 
     @Override
-
     protected void onPause(){
         super.onPause();
         Log.i(ACTIVITY_NAME,"In onPause()");
 
     }
     @Override
-
     protected void onStop(){
         super.onStop();
         Log.i(ACTIVITY_NAME,"In onStop()");
     }
 
     @Override
-
     protected void onDestroy(){
         super.onDestroy();
         Log.i(ACTIVITY_NAME,"In onDestroy()");
